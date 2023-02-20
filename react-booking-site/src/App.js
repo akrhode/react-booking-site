47dfa9c03ae2741cb998650d278aaf13c0de33b6
akrhode <a.rhode@web.de>
2023-02-20 22:40:16 +0100

    Build hero section

--- react-booking-site/src/App.js
+++ react-booking-site/src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
+import Hero from "./components/Hero.jsx";
 import Navbar from "./components/Navbar.jsx";
 
 function App() {
   return (
     <div>
       <Navbar />
+      <Hero />
     </div>
   );
 }
