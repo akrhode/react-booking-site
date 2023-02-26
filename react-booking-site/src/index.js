6ef44328c2d03fd2313660ae1a317a86b48f6532
akrhode <a.rhode@web.de>
2023-02-27 00:45:59 +0100

    Deployed

--- react-booking-site/src/index.js
+++ react-booking-site/src/index.js
@@ -2,13 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { createHashRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import BlogGreece from "./pages/BlogGreece.jsx";
 import BlogItaly from "./pages/BlogItaly.jsx";
 import BlogSpain from "./pages/BlogSpain.jsx";
 import BlogPortugal from "./pages/BlogPortugal.jsx";
 
-const router = createHashRouter([
+const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
